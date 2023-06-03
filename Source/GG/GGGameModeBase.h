8b49658f89e09a24156676c8ce19d720f4a57298
VladimirVL909 <56089596+VladimirVL909@users.noreply.github.com>
2023-06-04 01:41:25 +0300

    clean status project
    A first starting progict to work

---
+++ Source/GG/GGGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GGGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GG_API AGGGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
